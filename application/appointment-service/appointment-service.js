da35219841bea797802fad7f1933f202f66cd521
narendiransankar <narendiransankar1699@gmail.com>
2025-07-20 21:17:36 +0530

    Update appointment-service.js

--- application/appointment-service/appointment-service.js
+++ application/appointment-service/appointment-service.js
@@ -16,7 +16,7 @@ const express = require('express');
     
     app.get('/appointments', (req, res) => {
       res.json({ 
-        message: 'Appointments retrieved successfully',
+        message: 'new Appointments retrieved successfully',
         count: appointments.length,
         appointments: appointments 
       });
